ad75c9eeaa3713eb1427976fbb09e728e50a5d29
Bing Lu <112145032+BingluCS@users.noreply.github.com>
2026-04-05 07:41:48 +0800

    Enhance file handling and directory management
    Added functions for directory management, file reading, and writing. Updated main logic for handling input and output directories, and improved error handling.

--- pio_sz3.cpp
+++ pio_sz3.cpp
@@ -2,16 +2,130 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
+#ifndef OMPI_SKIP_MPICXX
+#define OMPI_SKIP_MPICXX 1
+#endif
+
+#ifndef MPICH_SKIP_MPICXX
+#define MPICH_SKIP_MPICXX 1
+#endif
+
 #include "SZ3/api/sz.hpp"
 #include "rw.h"
 #include "mpi.h"
 
-// USAGE:
-// mpirun -np 16 parallel sz.config folder_num r3 r2 r1
+static const char* INPUT_DIR = "/public/share/acnnprvuzd/data/sdrbench/NYX";
+static const char* OUTPUT_DIR = "/public/share/acnnprvuzd/data/out";
+
+static int ensure_dir(const char* dir)
+{
+    struct stat st;
+    if (stat(dir, &st) == 0) {
+        if (S_ISDIR(st.st_mode))
+            return 0;
+        errno = ENOTDIR;
+        return -1;
+    }
+    if (mkdir(dir, 0777) == 0)
+        return 0;
+    if (errno == EEXIST)
+        return 0;
+    return -1;
+}
+
+static void build_tmp_filename(char* filename, size_t filename_len, const char* prefix, int world_rank)
+{
+    const char* jobid = getenv("SLURM_JOB_ID");
+    if (jobid == NULL || jobid[0] == '\0')
+        jobid = getenv("PBS_JOBID");
+    if (jobid == NULL || jobid[0] == '\0')
+        jobid = "nojid";
+    snprintf(filename, filename_len, "%s/%s_%s_%d_%d_%ld.out",
+             OUTPUT_DIR, prefix, jobid, world_rank, (int)getpid(), (long)time(NULL));
+}
+
+static int write_all_bytes(const unsigned char* bytes, size_t byte_length, const char* filename)
+{
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    size_t written = 0;
+    if (fd < 0)
+        return -1;
+    while (written < byte_length) {
+        ssize_t count = write(fd, bytes + written, byte_length - written);
+        if (count < 0) {
+            if (errno == EINTR)
+                continue;
+            close(fd);
+            return -1;
+        }
+        if (count == 0) {
+            close(fd);
+            errno = EIO;
+            return -1;
+        }
+        written += (size_t)count;
+    }
+    if (close(fd) != 0)
+        return -1;
+    return 0;
+}
+
+static unsigned char* read_all_bytes(const char* filename, size_t* byte_length)
+{
+    int fd = open(filename, O_RDONLY);
+    struct stat st;
+    unsigned char* buf;
+    size_t total = 0;
+    if (fd < 0)
+        return NULL;
+    if (fstat(fd, &st) != 0) {
+        close(fd);
+        return NULL;
+    }
+    if (st.st_size < 0) {
+        close(fd);
+        errno = EIO;
+        return NULL;
+    }
+    *byte_length = (size_t)st.st_size;
+    buf = (unsigned char*)malloc((*byte_length == 0) ? 1 : *byte_length);
+    if (buf == NULL) {
+        close(fd);
+        errno = ENOMEM;
+        return NULL;
+    }
+    while (total < *byte_length) {
+        ssize_t count = read(fd, buf + total, *byte_length - total);
+        if (count < 0) {
+            if (errno == EINTR)
+                continue;
+            free(buf);
+            close(fd);
+            return NULL;
+        }
+        if (count == 0) {
+            free(buf);
+            close(fd);
+            errno = EIO;
+            return NULL;
+        }
+        total += (size_t)count;
+    }
+    if (close(fd) != 0) {
+        free(buf);
+        return NULL;
+    }
+    return buf;
+}
+
+
 int main(int argc, char * argv[])
 {
     srand(time(0));
@@ -33,22 +147,22 @@ int main(int argc, char * argv[])
         exit(0);
     }
 
-    //cfgFile=argv[1];
+
     cfgFile=NULL;
     double eb=atof(argv[2]);
     
     if(argc>=5)
-      r1 = atoi(argv[4]); //8
+      r1 = atoi(argv[4]);
     if(argc>=6)
-      r2 = atoi(argv[5]); //8
+      r2 = atoi(argv[5]); 
     if(argc>=7)
-      r3 = atoi(argv[6]); //128
+      r3 = atoi(argv[6]);
     if(argc>=8)
       r4 = atoi(argv[7]);
     if(argc>=9)
       r5 = atoi(argv[8]);
     
-//    SZ_Init(NULL);
+
 
     if (world_rank == 0) printf ("Start parallel compressing ... \n");
     if (world_rank == 0) printf("size: %d\n", world_size);
@@ -63,7 +177,7 @@ int main(int argc, char * argv[])
     char qmcpack8h_file[2][50] = {"spin_0_truncated.bin.dat", "spin_1_truncated.bin.dat"};
     double qmcpack8h_rel_bound[2] = {1e-6, 1e-6};
 
-    // qmacpack6k
+
     int qmcpack6k_num_vars = 20;
     char qmacpack6k_file[20][50] = {"s2700l300_truncated.bin.dat", "s4500l300_truncated.bin.dat", "s1200l300_truncated.bin.dat",
                                     "s300l300_truncated.bin.dat", "s4200l300_truncated.bin.dat", "s5400l300_truncated.bin.dat",
@@ -75,20 +189,16 @@ int main(int argc, char * argv[])
     double qmacpack6k_rel_bound[20] = {1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6,
                                        1e-6, 1e-6, 1e-6, 1e-6, 1e-6};
 
-    // Hurricane
+
     int hurricane_num_vars = 13;
-    /*char hurricane_file[13][50] = {"Uf48.bin.dat", "Vf48.bin.dat", "Wf48.bin.dat",
-                                   "TCf48.bin.dat", "Pf48.bin.dat", "QVAPORf48.bin.dat",
-                                   "CLOUDf48_log10.bin.dat", "QCLOUDf48_log10.bin.dat", "QICEf48_log10.bin.dat",
-                                   "QRAINf48_log10.bin.dat", "QSNOWf48_log10.bin.dat", "QGRAUPf48_log10.bin.dat",
-                                   "PRECIPf48_log10.bin.dat"};*/
+
     char hurricane_file[13][50] = {"Uf48.bin.f32", "Vf48.bin.f32", "Wf48.bin.f32",
                                    "TCf48.bin.f32", "Pf48.bin.f32", "QVAPORf48.bin.f32",
                                    "CLOUDf48.log10.bin.f32", "QCLOUDf48.log10.bin.f32", "QICEf48.log10.bin.f32",
                                    "QRAINf48.log10.bin.f32", "QSNOWf48.log10.bin.f32", "QGRAUPf48.log10.bin.f32",
                                    "PRECIPf48.log10.bin.f32"};    
 double hurricane_rel_bound[13] ={7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4,7e-4, 7e-4, 7e-4, 7e-4, 7e-4};
-    // miranda
+
     int miranda_num_vars = 7;
     char miranda_file[7][50] = {"velocityy_truncated.bin.dat", "velocityx_truncated.bin.dat", "density_truncated.bin.dat",
                                 "pressure_truncated.bin.dat", "velocityz_truncated.bin.dat", "viscocity_truncated.bin.dat",
@@ -103,24 +213,26 @@ double hurricane_rel_bound[13] ={7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4,
                                   };
     double scale_rel_bound[12] ={1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3};
     int nyx_num_vars = 6;
-    char nyx_file[6][50] = {"temperature.f32", "velocity_x.f32", "velocity_y.f32",
-                                "velocity_z.f32", "dark_matter_density.f32", "baryon_density.f32"};
+    /*char nyx_file[6][50] = {"temperature.f32", "velocity_x.f32", "velocity_y.f32",
+                                "velocity_z.f32", "dark_matter_density.f32", "baryon_density.f32"};*/
+    char nyx_file[6][50] = {"temperature.f32", "velocity_y.f32",
+                                "velocity_z.f32", "velocity_x.f32"};
     double nyx_rel_bound[7] = {1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3};
     int aramco_num_vars = 50;
     char aramco_file[50][50];
     double aramco_rel_bound[50];
     for (int i=0;i<50;i++){
-        //char name[50];
+
         sprintf(aramco_file[i],"aramco-snapshot-%d.f32",1000+10*i);
-        //salt_file[i]=name;
+
         aramco_rel_bound[i]=1e-3;
 
     }
 
-    // assignment
+
     char file[100][50];
     double *rel_bound;
-    //std::cout << "num_vars" << num_vars <<std::endl;
+
     if (num_vars == qmcpack6k_num_vars) {
         for (int i = 0; i < num_vars; i++) strcpy(file[i], qmacpack6k_file[i]);
         rel_bound = qmacpack6k_rel_bound;
@@ -147,16 +259,16 @@ double hurricane_rel_bound[13] ={7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4,
     }
     else {
         printf("No such variable, exit\n");
-//        SZ_Finalize();
+
         MPI_Finalize();
         return 0;
     }
     size_t compressed_size[20];
 
-    char folder[100] = "/home/x-jliu18/Jinyang/datasets/Hurricane";
-    char filename[100];
-    char zip_filename[100];
-    // char out_filename[100];
+    const char *folder = INPUT_DIR;
+    char filename[512];
+    char zip_filename[512];
+
     size_t inSize, outSize; 
     size_t nbEle;
     int status;
@@ -181,23 +293,22 @@ double hurricane_rel_bound[13] ={7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4,
         conf.loadcfg(cfgFile);
     }
     conf.errorBoundMode=SZ3::EB_REL;
+    num_vars=4;
     for(int i=0; i<num_vars; i++){
         sprintf(filename, "%s/%s", folder, file[i]);
-        //sprintf(filename, "%s/%d/%s", folder, folder_index, file[i]);
-        //std::cout << filename << std::endl;
-        //conf.relErrorBound = rel_bound[i];
+
         conf.relErrorBound = eb;
-        // Read Input Data
+
         if(world_rank == 0){
             start = MPI_Wtime();
             dataIn = readFloatData(filename, &nbEle, &status);
             end = MPI_Wtime();
-            //printf("file %s read time: %.2f\n", filename, end - start);
+
             start = MPI_Wtime();
             MPI_Bcast(&nbEle, 1, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
             MPI_Bcast(dataIn, nbEle, MPI_FLOAT, 0, MPI_COMM_WORLD);
             end = MPI_Wtime();
-            //printf("broadcast time: %.2f\n", end - start);
+
 
         }
         else{
@@ -211,16 +322,15 @@ double hurricane_rel_bound[13] ={7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4,
             costReadOri += end - start;
         }
         
-        // Compress Input Data
+
         size_t out_size;
 
-        //if (world_rank == 0) printf ("Compressing %s\n", filename);
+
         MPI_Barrier(MPI_COMM_WORLD);
         if(world_rank == 0) start = MPI_Wtime();
 
         char *bytesOut = SZ_compress<float>(conf, dataIn, compressed_size[i]);
-       // printf ("Compressing %d end.\n", world_rank);
-//        unsigned char *bytesOut = SZ_compress_args(SZ_FLOAT, dataIn, &compressed_size[i], REL, 0, rel_bound[i], 0, r5, r4, r3, r2, r1);
+
         MPI_Barrier(MPI_COMM_WORLD);
         if(world_rank == 0){
             end = MPI_Wtime();
@@ -228,26 +338,33 @@ double hurricane_rel_bound[13] ={7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4,
         }
         free (dataIn);
         memcpy(compressed_output_pos, bytesOut, compressed_size[i]);
-       // printf ("memcpy %d end.\n", world_rank);
+
       
         compressed_output_pos += compressed_size[i];
-        //free(bytesOut);
+
       
     }
-    //printf ("total %d end.\n", world_rank);
-    struct stat st = {0};
-    if (stat("/home/x-jliu18/Jinyang/datasets/tmp", &st) == -1) {
-        mkdir("/home/x-jliu18/Jinyang/datasets/tmp", 0777);
+
+    if (ensure_dir(OUTPUT_DIR) != 0) {
+        printf("ERROR! Failed to prepare output dir %s: %s\n", OUTPUT_DIR, strerror(errno));
+        free(compressed_output);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+        return 1;
     }
-    sprintf(zip_filename, "%s/sz3_%d_%d.out", "/home/x-jliu18/Jinyang/datasets/tmp", folder_index, rand());    // Write Compressed Data
+    build_tmp_filename(zip_filename, sizeof(zip_filename), "sz3", folder_index);
     size_t total_size = compressed_output_pos - compressed_output;
   
-    // Write Compressed Data
+
     MPI_Barrier(MPI_COMM_WORLD);
-    //if (world_rank == 0) printf("write compressed file to disk %s \n", zip_filename);
+
     if(world_rank == 0) start = MPI_Wtime();
-    writeByteData(compressed_output, total_size, zip_filename, &status);
-    //printf ("write %d end.\n", world_rank);
+    if (write_all_bytes(compressed_output, total_size, zip_filename) != 0) {
+        printf("ERROR! Failed to write compressed file %s: %s\n", zip_filename, strerror(errno));
+        free(compressed_output);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+        return 1;
+    }
+
   
     MPI_Barrier(MPI_COMM_WORLD);
     if(world_rank == 0){
@@ -255,14 +372,19 @@ double hurricane_rel_bound[13] ={7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4,
         costWriteZip += end - start;
     }
     free(compressed_output);
-    // Read Compressed Data
+
     MPI_Barrier(MPI_COMM_WORLD);
-    //if (world_rank == 0) printf("read compressed file from disk %s \n", zip_filename);
+
     if(world_rank == 0) start = MPI_Wtime();
-    compressed_output = readByteData(zip_filename, &inSize, &status);
-   // printf ("read %d end.\n", world_rank);
+    compressed_output = read_all_bytes(zip_filename, &inSize);
+    if (compressed_output == NULL) {
+        printf("ERROR! Failed to read compressed file %s: %s\n", zip_filename, strerror(errno));
+        MPI_Abort(MPI_COMM_WORLD, 1);
+        return 1;
+    }
+
     if (inSize != total_size) {
-        printf("ERROR! Broken file : %s", zip_filename);
+        printf("ERROR! Broken file : %s (expected %zu bytes, got %zu bytes)\n", zip_filename, total_size, inSize);
     } else {
         remove(zip_filename);
     }
@@ -276,21 +398,19 @@ double hurricane_rel_bound[13] ={7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4,
 
 
     for(int i=0; i<num_vars; i++){
-        // Decompress Compressed Data
+
         MPI_Barrier(MPI_COMM_WORLD);
-        //if (world_rank == 0) printf("decompress %d-th field\n", i);
+
         if(world_rank == 0) start = MPI_Wtime();
         float *dataOut = SZ_decompress<float>(conf,(char*)compressed_output_pos, compressed_size[i]);
-        //printf ("decomp %d end.\n", world_rank);
-    
-//        float *dataOut = SZ_decomprescs(SZ_FLOAT, compressed_output_pos, compressed_size[i], r5, r4, r3, r2, r1);
+
         MPI_Barrier(MPI_COMM_WORLD);
         if(world_rank == 0){
             end = MPI_Wtime();
             costDecomp += end - start; 
         }
         compressed_output_pos += compressed_size[i];
-        //free(dataOut);
+
     }
     free(compressed_output);
 
@@ -315,7 +435,7 @@ double hurricane_rel_bound[13] ={7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4,
     }
 
 
-//    SZ_Finalize();
+
 
     MPI_Finalize();
 
