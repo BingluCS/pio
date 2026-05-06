833404111e12c3a977a37e548196ecb3b8e22a7b
Bing Lu <112145032+BingluCS@users.noreply.github.com>
2026-05-07 01:25:26 +0800

    Refactor input handling and update dataset support

--- pio_pfpl.cpp
+++ pio_pfpl.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <time.h>
@@ -12,8 +13,7 @@
 
 #include "pfpl_f32_noa_cpu_api.hpp"
 #include "mpi.h"
-
-static const char* INPUT_DIR = "/public/share/acnnprvuzd/data/sdrbench/NYX";
+#include "rw.h"
 static const char* OUTPUT_DIR = "/public/share/acnnprvuzd/data/out";
 
 static int ensure_dir(const char* dir)
@@ -121,8 +121,7 @@ static unsigned char* read_all_bytes(const char* filename, size_t* byte_length)
 int main(int argc, char * argv[])
 {
     srand(time(0));
-    size_t r5=0,r4=0,r3=0,r2=0,r1=0;
-    char *cfgFile;
+    size_t r1 = 0, r2 = 0, r3 = 0;
 
     MPI_Init(NULL, NULL);
 
@@ -132,27 +131,18 @@ int main(int argc, char * argv[])
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    if(argc < 4)
+    if(argc < 5)
     {
-        printf("Test case: parallel_pfpl [unused] error_bound num_vars [dimension sizes...]\n");
-        printf("Example: parallel_pfpl none 1e-3 7 384 384 256\n");
+        printf("Test case: parallel_pfpl error_bound dataset_name input_dir nums\n");
+        printf("Example: parallel_pfpl 1e-3 nyx /public/share/acnnprvuzd/data/sdrbench/NYX 4\n");
         MPI_Finalize();
         return 0;
     }
 
-    cfgFile = NULL;
-    double eb = atof(argv[2]);
-
-    if(argc>=5)
-      r1 = atoi(argv[4]);
-    if(argc>=6)
-      r2 = atoi(argv[5]);
-    if(argc>=7)
-      r3 = atoi(argv[6]);
-    if(argc>=8)
-      r4 = atoi(argv[7]);
-    if(argc>=9)
-      r5 = atoi(argv[8]);
+    double eb = atof(argv[1]);
+    const char *dataset_name = argv[2];
+    const char *folder = argv[3];
+    int num_vars = atoi(argv[4]);
 
     if (world_rank == 0) printf ("Start parallel compressing ... \n");
     if (world_rank == 0) printf("size: %d\n", world_size);
@@ -160,22 +150,6 @@ int main(int argc, char * argv[])
     double costReadOri = 0.0, costReadZip = 0.0, costWriteZip = 0.0, costWriteOut = 0.0, costComp = 0.0, costDecomp = 0.0;
 
     MPI_Barrier(MPI_COMM_WORLD);
-    int num_vars = atoi(argv[3]);
-
-    int qmcpack8h_num_vars = 2;
-    char qmcpack8h_file[2][50] = {"spin_0_truncated.bin.dat", "spin_1_truncated.bin.dat"};
-    double qmcpack8h_rel_bound[2] = {1e-6, 1e-6};
-
-    int qmcpack6k_num_vars = 20;
-    char qmacpack6k_file[20][50] = {"s2700l300_truncated.bin.dat", "s4500l300_truncated.bin.dat", "s1200l300_truncated.bin.dat",
-                                    "s300l300_truncated.bin.dat", "s4200l300_truncated.bin.dat", "s5400l300_truncated.bin.dat",
-                                    "s1800l300_truncated.bin.dat", "s5700l300_truncated.bin.dat", "s4800l300_truncated.bin.dat",
-                                    "s3300l300_truncated.bin.dat", "s5100l300_truncated.bin.dat", "s1500l300_truncated.bin.dat",
-                                    "s600l300_truncated.bin.dat", "s0l300_truncated.bin.dat", "s3600l300_truncated.bin.dat",
-                                    "s900l300_truncated.bin.dat", "s3900l300_truncated.bin.dat", "s3000l300_truncated.bin.dat",
-                                    "s2100l300_truncated.bin.dat", "s2400l300_truncated.bin.dat"};
-    double qmacpack6k_rel_bound[20] = {1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6, 1e-6,
-                                       1e-6, 1e-6, 1e-6, 1e-6, 1e-6};
 
     int hurricane_num_vars = 13;
     char hurricane_file[13][50] = {"Uf48.bin.f32", "Vf48.bin.f32", "Wf48.bin.f32",
@@ -183,71 +157,58 @@ int main(int argc, char * argv[])
                                    "CLOUDf48.log10.bin.f32", "QCLOUDf48.log10.bin.f32", "QICEf48.log10.bin.f32",
                                    "QRAINf48.log10.bin.f32", "QSNOWf48.log10.bin.f32", "QGRAUPf48.log10.bin.f32",
                                    "PRECIPf48.log10.bin.f32"};
-    double hurricane_rel_bound[13] ={7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4, 7e-4,7e-4, 7e-4, 7e-4, 7e-4, 7e-4};
-
-    int miranda_num_vars = 7;
-    char miranda_file[7][50] = {"velocityy_truncated.bin.dat", "velocityx_truncated.bin.dat", "density_truncated.bin.dat",
-                                "pressure_truncated.bin.dat", "velocityz_truncated.bin.dat", "viscocity_truncated.bin.dat",
-                                "diffusivity_truncated.bin.dat"};
-    double miranda_rel_bound[7] = {1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3};
 
     int scale_num_vars = 12;
-    char scale_file[12][50] = {"PRES-98x1200x1200.dat", "QC-98x1200x1200.log10.dat", "QG-98x1200x1200.log10.dat",
-                               "QI-98x1200x1200.log10.dat", "QR-98x1200x1200.log10.dat", "QS-98x1200x1200.log10.dat",
-                               "QV-98x1200x1200.log10.dat", "RH-98x1200x1200.dat", "T-98x1200x1200.dat",
-                               "U-98x1200x1200.dat", "V-98x1200x1200.dat", "W-98x1200x1200.dat"};
-    double scale_rel_bound[12] ={1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3};
+    char scale_file[12][50] = {"PRES-98x1200x1200.f32", "QC-98x1200x1200.f32", "QG-98x1200x1200.f32",
+                               "QI-98x1200x1200.f32", "QR-98x1200x1200.f32", "QS-98x1200x1200.f32",
+                               "QV-98x1200x1200.f32", "RH-98x1200x1200.f32", "T-98x1200x1200.f32",
+                               "U-98x1200x1200.f32", "V-98x1200x1200.f32", "W-98x1200x1200.f32"};
 
     int nyx_num_vars = 6;
-    char nyx_file[6][50] = {"temperature.f32","velocity_y.f32",
-                            "velocity_z.f32", "velocity_x.f32"};
-    double nyx_rel_bound[7] = {1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3, 1e-3};
+    char nyx_file[6][50] = {"temperature.f32", "velocity_y.f32", "velocity_z.f32", "velocity_x.f32", "dark_matter_density.f32", "baryon_density.f32"};
 
-    int aramco_num_vars = 50;
-    char aramco_file[50][50];
-    double aramco_rel_bound[50];
-    for (int i=0;i<50;i++){
-        sprintf(aramco_file[i],"aramco-snapshot-%d.f32",1000+10*i);
-        aramco_rel_bound[i]=1e-3;
-    }
-
+	int jhtdb_num_vars = 10;
+    	char jhtdb_file[10][50] = {"0500_pressure.f32", "1000_pressure.f32", "1500_pressure.f32", "2000_pressure.f32", "2500_pressure.f32", "3000_pressure.f32",
+                                    "3500_pressure.f32", "4000_pressure.f32", "4500_pressure.f32", "5000_pressure.f32"};
     char file[100][50];
-    double *rel_bound;
-    if (num_vars == qmcpack6k_num_vars) {
-        for (int i = 0; i < num_vars; i++) strcpy(file[i], qmacpack6k_file[i]);
-        rel_bound = qmacpack6k_rel_bound;
-    } else if (num_vars == qmcpack8h_num_vars) {
-        for (int i = 0; i < num_vars; i++) strcpy(file[i], qmcpack8h_file[i]);
-        rel_bound = qmcpack8h_rel_bound;
-    } else if (num_vars == hurricane_num_vars) {
-        for (int i = 0; i < num_vars; i++) strcpy(file[i], hurricane_file[i]);
-        rel_bound = hurricane_rel_bound;
-    } else if (num_vars == miranda_num_vars) {
-        for (int i = 0; i < num_vars; i++) strcpy(file[i], miranda_file[i]);
-        rel_bound = miranda_rel_bound;
-    } else if (num_vars == nyx_num_vars) {
-        for (int i = 0; i < num_vars; i++) strcpy(file[i], nyx_file[i]);
-        rel_bound = nyx_rel_bound;
-    } else if (num_vars == scale_num_vars) {
-        for (int i = 0; i < num_vars; i++) strcpy(file[i], scale_file[i]);
-        rel_bound = scale_rel_bound;
-    } else if (num_vars == aramco_num_vars) {
-        for (int i = 0; i < num_vars; i++) strcpy(file[i], aramco_file[i]);
-        rel_bound = aramco_rel_bound;
+    int dataset_num_vars = 0;
+    if (strcasecmp(dataset_name, "hurricane") == 0) {
+        dataset_num_vars = hurricane_num_vars;
+        r1 = 500;
+        r2 = 500;
+        r3 = 100;
+        for (int i = 0; i < dataset_num_vars; i++) strcpy(file[i], hurricane_file[i]);
+    } else if (strcasecmp(dataset_name, "jhtdb") == 0) {
+        dataset_num_vars = jhtdb_num_vars;
+        r1 = 512;
+        r2 = 512;
+        r3 = 512;
+        for (int i = 0; i < dataset_num_vars; i++) strcpy(file[i], jhtdb_file[i]);
+    } else if (strcasecmp(dataset_name, "nyx") == 0) {
+        dataset_num_vars = nyx_num_vars;
+        r1 = 512;
+        r2 = 512;
+        r3 = 512;
+        for (int i = 0; i < dataset_num_vars; i++) strcpy(file[i], nyx_file[i]);
+    } else if (strcasecmp(dataset_name, "scale") == 0) {
+        dataset_num_vars = scale_num_vars;
+        r1 = 1200;
+        r2 = 1200;
+        r3 = 98;
+        for (int i = 0; i < dataset_num_vars; i++) strcpy(file[i], scale_file[i]);
     } else {
-        printf("No such variable, exit\n");
+        printf("Unsupported dataset %s, only hurricane/nyx/scale are supported\n", dataset_name);
         MPI_Finalize();
         return 0;
     }
-
     size_t compressed_size[100];
     size_t original_size[100];
 
-    const char *folder = INPUT_DIR;
     char filename[512];
     char zip_filename[512];
     size_t inSize;
     size_t nbEle;
+    size_t expected_nbEle = r1 * r2 * r3;
     size_t total_original_size = 0;
     size_t total_size = 0;
     int status;
@@ -259,16 +220,24 @@ int main(int argc, char * argv[])
     int folder_index = world_rank;
     float threshold = INFINITY;
 
-    if (cfgFile != NULL) {
-        threshold = (float)atof(cfgFile);
-    }
-    num_vars = 4;
     for(int i=0; i<num_vars; i++){
         sprintf(filename, "%s/%s", folder, file[i]);
 
         if(world_rank == 0){
             start = MPI_Wtime();
             dataIn = readFloatData(filename, &nbEle, &status);
+            if (dataIn == NULL || nbEle == 0) {
+                printf("ERROR! Failed to read input file %s\n", filename);
+                MPI_Abort(MPI_COMM_WORLD, 1);
+                return 1;
+            }
+            if (nbEle != expected_nbEle) {
+                printf("ERROR! Dimension mismatch for %s: file has %zu elements, but dataset %s expects %zu (%zu x %zu x %zu)\n",
+                       filename, nbEle, dataset_name, expected_nbEle, r1, r2, r3);
+                free(dataIn);
+                MPI_Abort(MPI_COMM_WORLD, 1);
+                return 1;
+            }
             start = MPI_Wtime();
             MPI_Bcast(&nbEle, 1, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
             MPI_Bcast(dataIn, nbEle, MPI_FLOAT, 0, MPI_COMM_WORLD);
@@ -370,7 +339,7 @@ int main(int argc, char * argv[])
 
     if (world_rank == 0)
     {
-        printf ("PFPL Finish parallel compressing, total compression ratio %.4g.\n", 1.0 * total_original_size / total_size);
+        printf ("PFPL Finish parallel compressing on %s, total compression ratio %.4g.\n", dataset_name, 1.0 * total_original_size / total_size);
         printf("Separate ratios: ");
         for(int i=0; i<num_vars; i++){
             printf("%.4g ", 1.0 * original_size[i] / compressed_size[i]);
